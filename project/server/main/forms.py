30e0b2202d50704ecdd4192010a98c1c18b038bd
David Ambartsumian <de1f364@gmail.com>
2020-05-19 16:01:12 +0300

    db setup

--- project/server/main/forms.py
+++ project/server/main/forms.py
@@ -2,15 +2,27 @@
 
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, SelectField
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 
+from project.server import bcrypt, db
+from project.server.models import Addrgroup, Appgroup
+
+
 class AddressForm(FlaskForm):
-    name = StringField("Address groupname", [DataRequired()])
-    addresses = StringField("Addresses", [DataRequired()])
+    name_addrs = StringField("Address groupname")
+    addresses = StringField("Addresses")
 
 
 class AppForm(FlaskForm):
-    name = StringField("Apps groupname", [DataRequired()])
-    apps = StringField("Applications", [DataRequired()])
+    name_apps = StringField("Apps groupname")
+    apps = StringField("Applications")
+
+
+class SelectAddrForm(FlaskForm):
+    addr_groups = QuerySelectField(query_factory=lambda: Addrgroup.query.all(), get_label="name")
+
+class SelectAppForm(FlaskForm):
+    app_groups = QuerySelectField(query_factory=lambda: Appgroup.query.all(), get_label="name")
--- project/server/main/views.py
+++ project/server/main/views.py
@@ -5,34 +5,53 @@ from flask import render_template, Blueprint, request, flash, redirect, url_for
 
 from project.server import bcrypt, db
 from project.server.models import Addrgroup, Appgroup
-from project.server.main.forms import AddressForm, AppForm
+from project.server.main.forms import AddressForm, AppForm, SelectAddrForm, SelectAppForm
 
 main_blueprint = Blueprint("main", __name__)
 
 
 @main_blueprint.route("/")
 def home():
-    return render_template("main/home.html")
+    addr_form = AddressForm()
+    app_form = AppForm()
+    select_addr_form = SelectAddrForm()
+    select_app_form = SelectAppForm()
+    return render_template("main/home.html",
+                           addr_form=addr_form,
+                           app_form=app_form,
+                           select_addr_form=select_addr_form,
+                           select_app_form=select_app_form)
 
 
 @main_blueprint.route("/about/")
 def about():
+    flash(Addrgroup.query.with_entities(Addrgroup.name))
     return render_template("main/about.html")
 
 
-@main_blueprint.route("/handle_data", methods=["POST"])
-def handle_data():
-    form = AddressForm(request.form)
-    print(form)
-    if form.validate_on_submit():
-        group_name = Addrgroup(name=form.name.data, addresses=form.addresses.data)
+@main_blueprint.route("/handle_data_addr", methods=["GET", "POST"])
+def handle_data_addr():
+    addr_form = AddressForm(request.form)
+    if addr_form.validate_on_submit():
+        group_name = Addrgroup(name=addr_form.name_addrs.data, addresses=addr_form.addresses.data)
         db.session.add(group_name)
         db.session.commit()
 
 
-        flash("Address group added")
-     g   return redirect(url_for("main.home"))
-    # group_name = request.form['groupName']
-    # address_list = request.form['addressList']
+        flash("Address group added", "success")
+        return redirect(url_for("main.home"))
     return redirect(url_for("main.about"))
 
+
+@main_blueprint.route("/handle_data_app", methods=["GET", "POST"])
+def handle_data_app():
+    app_form = AppForm(request.form)
+    if app_form.validate_on_submit():
+        group_name = Appgroup(name=app_form.name_apps.data, apps=app_form.apps.data)
+        db.session.add(group_name)
+        db.session.commit()
+
+
+        flash("App group added", "success")
+        return redirect(url_for("main.home"))
+    return redirect(url_for("main.about"))
